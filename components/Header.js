bd1264d6808807f6281282c2a6b8fa9445ef60de
Helen Feng <53279829+hailenwashere@users.noreply.github.com>
2024-09-22 23:05:23 -0700

    file structure + change minor button css

--- components/Header.js
+++ components/Header.js
@@ -46,7 +46,7 @@ class Header extends HTMLElement {
               <li><a href="index.html">Home</a></li>
               <li><a href="about.html">About</a></li>
               <li><a href="contact.html">Contact</a></li>
-              <li><a href="https://docs.google.com/forms/d/e/1FAIpQLSeBjPGxwt7D1XYR6ewPVUk0R59oDKLASUUjJCu4ki1wRcWT6Q/viewform?usp=sf_link" class="button1">Stay Updated!</a></li>
+              <li><a href="https://docs.google.com/forms/d/e/1FAIpQLSeBjPGxwt7D1XYR6ewPVUk0R59oDKLASUUjJCu4ki1wRcWT6Q/viewform?usp=sf_link">Stay Updated!</a></li>
               </ul>
           </nav>
         </header>
